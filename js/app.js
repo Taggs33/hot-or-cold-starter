033fe65333f4c6b33899dfd86260f62545c149d7
Taggs33 <taggart3317@gmail.com>
2015-12-03 17:51:39 -0500

    set random number function

--- js/app.js
+++ js/app.js
@@ -1,6 +1,10 @@
+var randomNumber = 0;
+var guessCount = 0;
 
 $(document).ready(function(){
 	
+	randomNumber = setRandomNumber();
+
 	/*--- Display information modal box ---*/
   	$(".what").click(function(){
     	$(".overlay").fadeIn(1000);
@@ -13,6 +17,33 @@ $(document).ready(function(){
   		$(".overlay").fadeOut(1000);
   	});
 
+  	$('.input-form').submit(function(event) {
+  		event.preventDefault();
+
+  		increaseGuessCount();
+
+  		clearGuess();	
+
+  	});
+
 });
 
+function setRandomNumber() {
+	var tempRandom = Math.floor((Math.random() * 100) + 1);
+	console.log('The Random Number Is ' + tempRandom);
+	return tempRandom;
+}
 
+function increaseGuessCount() {
+	// guessCount = guessCount + 1;
+	guessCount++;
+	console.log('The Guess Count Is ' + guessCount);
+	$('#count').text(guessCount);
+} 
+
+function clearGuess() {
+	$('#userGuess').val('');
+}
+
+// Math.abs( whatever ends up being here becomes positive )
+// difference = users guess - secret numbe
